6806796b94491d1d03ea7eeea4ac4ababccf4c5a
Bartek1244 <bmatuszewski.bm@gmail.com>
2025-04-29 19:27:57 +0200

    improve taks1 database operations

--- apbd_cw7_s30522/apbd_cw7_s30522/Services/DbService.cs
+++ apbd_cw7_s30522/apbd_cw7_s30522/Services/DbService.cs
@@ -12,61 +12,63 @@ public class DbService(IConfiguration config) : IDbService
 {
     private readonly string _connectionString = config.GetConnectionString("Default");
 
-    public async Task<IEnumerable<TripCountriesGetDTO>> GetAllTripsWithCountriesAsync()
+     public async Task<IEnumerable<TripCountriesGetDTO>> GetAllTripsWithCountriesAsync()
     {
-        var result = new List<TripCountriesGetDTO>();
+        var tripsWithCountries = new List<TripCountriesGetDTO>();
+        var countries = new List<CountryGetDTO>();
         
         await using var connection = new SqlConnection(_connectionString);
-
-        string queryIdTrip = "SELECT IdTrip FROM Country_Trip";
-        await using var commandIdTrip = new SqlCommand(queryIdTrip, connection);
-
         await connection.OpenAsync();
-        await using var readerIdTrip = await commandIdTrip.ExecuteReaderAsync();
-
-        while (await readerIdTrip.ReadAsync())
+        
+        string queryCountries = "SELECT IdCountry, Name FROM Country";
+        await using var commandCountries = new SqlCommand(queryCountries, connection);
+        await using (var readerCountries = await commandCountries.ExecuteReaderAsync())
         {
-            int idTrip = readerIdTrip.GetInt32(0);
-
-            string queryTrip = "SELECT IdTrip, Name, Description, DateFrom, DateTo, MaxPeople " +
-                               "FROM Trip " +
-                               $"WHERE IdTrip = {idTrip} ";
-
-            await using var commandTrip = new SqlCommand(queryTrip, connection);
-            await using var readerTrip = await commandTrip.ExecuteReaderAsync();
-            await readerTrip.ReadAsync();
-
-            var tripCountriesDTO = new TripCountriesGetDTO
+            while (await readerCountries.ReadAsync())
             {
-                IdTrip = readerTrip.GetInt32(0),
-                Name = readerTrip.GetString(1),
-                Description = readerTrip.GetString(2),
-                DateFrom = readerTrip.GetDateTime(3),
-                DateTo = readerTrip.GetDateTime(4),
-                MaxPeople = readerTrip.GetInt32(5),
-                Countries = new List<CountryGetDTO>()
-            };
-
-            string queryCountry = "SELECT c.IdCountry, c.Name " +
-                                  "FROM Country c " +
-                                  "JOIN Country_Trip c_t ON c.IdCountry = c_t.IdCountry " +
-                                  $"WHERE c_t.IdTrip = {idTrip}";
-
-            await using var commandCountry = new SqlCommand(queryCountry, connection);
-            await using var readerCountry = await commandCountry.ExecuteReaderAsync();
-
-            while (await readerCountry.ReadAsync())
-            {
-                tripCountriesDTO.Countries.Add(new CountryGetDTO
+                countries.Add(new CountryGetDTO
                 {
-                    IdCountry = readerCountry.GetInt32(0),
-                    Name = readerCountry.GetString(1)
+                    IdCountry = readerCountries.GetInt32(0),
+                    Name = readerCountries.GetString(1)
                 });
             }
-            
-            result.Add(tripCountriesDTO);
         }
+        
+        string queryTrips = "SELECT IdTrip, Name, Description, DateFrom, DateTo, MaxPeople FROM Trip";
+        await using var commandTrips = new SqlCommand(queryTrips, connection);
+        await using (var readerTrips = await commandTrips.ExecuteReaderAsync())
+        {
+            while (await readerTrips.ReadAsync())
+            {
+                tripsWithCountries.Add(new TripCountriesGetDTO
+                {
+                    IdTrip = readerTrips.GetInt32(0),
+                    Name = readerTrips.GetString(1),
+                    Description = readerTrips.GetString(2),
+                    DateFrom = readerTrips.GetDateTime(3),
+                    DateTo = readerTrips.GetDateTime(4),
+                    MaxPeople = readerTrips.GetInt32(5),
+                    Countries = new List<CountryGetDTO>()
+                });
+            }
+        }
+        
+        string queryCountriesTrips = "SELECT IdCountry, IdTrip FROM Country_Trip";
+        await using var commandCountriesTrips = new SqlCommand(queryCountriesTrips, connection);
+        await using (var readerCountriesTrips = await commandCountriesTrips.ExecuteReaderAsync())
+        {
 
-        return result;
+            while (await readerCountriesTrips.ReadAsync())
+            {
+                int idCountry = readerCountriesTrips.GetInt32(0);
+                int idTrip = readerCountriesTrips.GetInt32(1);
+
+                tripsWithCountries.Find(t => t.IdTrip == idTrip)
+                    .Countries.Add(countries.Find(c => c.IdCountry == idCountry));
+            }
+        }
+        
+        return tripsWithCountries;
     }
+     
 }
