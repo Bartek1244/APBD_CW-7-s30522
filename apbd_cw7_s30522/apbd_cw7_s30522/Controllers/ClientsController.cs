dc368d9b7118c8a44e22ec4b98daf507fec47277
Bartek1244 <bmatuszewski.bm@gmail.com>
2025-04-29 17:07:57 +0200

    init project

---
+++ apbd_cw7_s30522/apbd_cw7_s30522/Controllers/ClientsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace apbd_cw7_s30522.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ClientsController(IConfiguration config) : ControllerBase
+{
+    
+}
